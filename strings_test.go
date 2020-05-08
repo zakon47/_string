03ec283fa86ac3d259cb8a2b55ed49d612e34913
3AK <zakon47@gmail.com>
2020-05-08 16:47:35 +0300

    init

---
+++ strings_test.go
@@ -0,0 +1,77 @@
+package _strings_test
+
+import (
+	"github.com/stretchr/testify/assert"
+	"github.com/zakon47/_strings"
+	"testing"
+)
+
+func TestNumberLeft(t *testing.T) {
+	data := []struct {
+		name   string
+		metka  string
+		result []string
+	}{
+		{name: "test1", metka: " 1m", result: []string{"1","m"}},
+		{name: "test1", metka: "2 1m", result: []string{"2"," 1m"}},
+		{name: "test1", metka: "4dsd", result: []string{"4","dsd"}},
+		{name: "test1", metka: "dsd4", result: []string{"","dsd4"}},
+	}
+	for _, test := range data {
+		t.Run(test.name, func(t *testing.T) {
+			assert.Equal(t, test.result, _strings.NumberLeft(test.metka))
+		})
+	}
+}
+func TestNumberRight(t *testing.T) {
+	data := []struct {
+		name   string
+		metka  string
+		result []string
+	}{
+		{name: "test1", metka: " 1m", result: []string{"","1m"}},
+		{name: "test1", metka: "2 1m", result: []string{"","2 1m"}},
+		{name: "test1", metka: "4dsd", result: []string{"","4dsd"}},
+		{name: "test1", metka: "dsd4", result: []string{"4","dsd"}},
+		{name: "test1", metka: "zak47", result: []string{"47","zak"}},
+	}
+	for _, test := range data {
+		t.Run(test.name, func(t *testing.T) {
+			assert.Equal(t, test.result, _strings.NumberRight(test.metka))
+		})
+	}
+}
+func TestStringLeft(t *testing.T) {
+	data := []struct {
+		name   string
+		metka  string
+		result []string
+	}{
+		{name: "test1", metka: " 1m", result: []string{"","1m"}},
+		{name: "test1", metka: "2 1m", result: []string{"","2 1m"}},
+		{name: "test1", metka: "4dsd", result: []string{"","4dsd"}},
+		{name: "test1", metka: "dsd4", result: []string{"dsd","4"}},
+	}
+	for _, test := range data {
+		t.Run(test.name, func(t *testing.T) {
+			assert.Equal(t, test.result, _strings.StringLeft(test.metka))
+		})
+	}
+}
+func TestStringRight(t *testing.T) {
+	data := []struct {
+		name   string
+		metka  string
+		result []string
+	}{
+		{name: "test1", metka: " 1m", result: []string{"m","1"}},
+		{name: "test1", metka: "2 1m", result: []string{"m","2 1"}},
+		{name: "test1", metka: "4dsd", result: []string{"dsd","4"}},
+		{name: "test1", metka: "dsd4", result: []string{"","dsd4"}},
+	}
+	for _, test := range data {
+		t.Run(test.name, func(t *testing.T) {
+			assert.Equal(t, test.result, _strings.StringRight(test.metka))
+		})
+	}
+}
---
+++ test/main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"github.com/zakon47/_strings"
+	"log"
+)
+
+func main()  {
+	log.Println(_strings.StringLeft("xaxa33xaxa"))
+}
